d565e855052eb9eece4c014958b091302b3a0166
mononoawaree <sk8l1f3111@gmail.com>
2026-09-16 00:01:42 -0700

    Dataset class done

---
+++ scripts/ImagesDataset.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+
+import PIL
+import torch
+from torch.utils.data import Dataset
+from torchvision.io import decode_image
+import clip
+from PIL import Image
+
+#Util class for DataLoader
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+ROOT = Path(r"D:\GrailedImages")
+
+class ImagesOnDiscDataset(Dataset):
+    def __init__(self, photo_keys, ids, transform=preprocess, target_transform=None):
+        self.photo_keys = photo_keys
+        self.ids = ids
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.photo_keys)
+
+    def __getitem__(self, idx):
+        key = self.photo_keys[idx]
+        img_path = ROOT / key[:2] / key[2:4] / f"{key}.jpg"
+        try:
+            image = PIL.Image.open(img_path)
+            image = image.convert("RGB")
+            image = self.transform(image)
+        except Exception as e:
+            print(f'{self.ids[idx]} Failed with exception {e}')
+            return torch.zeros(3, 224, 224), self.ids[idx], False
+        return image, self.ids[idx], True
+
+def main():
+    print(clip.available_models())
+if __name__ == '__main__':
+    main()
--- scripts/embed_pictures.py
+++ scripts/embed_pictures.py
@@ -1,5 +1,13 @@
-import torch
-from torch import nn
 from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import v2
+import polars as pl
+
+from scripts.ImagesDataset import ImagesOnDiscDataset
+
+
+def embed_pictures():
+    df = pl.scan_parquet('../data/parquets/sold_listings_20260901.parquet')
+    ids_keys = df.select(pl.col('photo_keys'), pl.col('id')).collect()
+    ids = ids_keys['id'].to_numpy()
+    keys = ids_keys['photo_keys'].to_numpy()
+    dataset = ImagesOnDiscDataset(keys, ids)
+    loader = DataLoader(dataset, batch_size=1000, num_workers=8, shuffle=False, pin_memory=True)
